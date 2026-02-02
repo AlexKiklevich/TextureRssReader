06a886b94f8065cdf21a512fae6c2cf0a674ff54
Aliaksandr Kiklevich <aliaksandr.kiklevich@tim.ws>
2026-02-02 15:28:07 +0300

    First steps. Added Project folder with Pods (Texture, Realm)

---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, '16.0'
+
+target 'TextureRssReader' do
+  use_frameworks!
+
+  pod 'RealmSwift'
+  pod 'Texture'
+
+  target 'TextureRssReaderTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TextureRssReaderUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ TextureRssReader/Modules/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TextureRssReader
+//
+//  Created by Aliaksandr Kiklevich on 2.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0.80, green: 0.95, blue: 0.80, alpha: 1.0)
+    }
+
+
+}
